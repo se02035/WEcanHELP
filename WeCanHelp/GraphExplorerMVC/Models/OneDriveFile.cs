5cb9be336438e17f1d1bcd5f2363203641a67b48
Oliver Lintner <oliver.lintner@outlook.com>
2015-10-23 21:48:24 +0200

    added onedrive for business file access (incl. file download) uploading file to azure storage account

---
+++ WeCanHelp/GraphExplorerMVC/Models/OneDriveFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExplorerMVC.Models
+{
+    public class OneDriveFile
+    {
+        public string Name { get; set; }
+        public string SourceId { get; set; }
+        public string WebUrl { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
