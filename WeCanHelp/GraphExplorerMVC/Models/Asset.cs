d238c9141d42e76ecfa2a55fbe2513a2554d0c6d
Oliver Lintner <oliver.lintner@outlook.com>
2015-10-23 18:08:53 +0200

    added new data field on the asset type. modify it to be locally executable. updated migrations

--- WeCanHelp/GraphExplorerMVC/Models/Asset.cs
+++ WeCanHelp/GraphExplorerMVC/Models/Asset.cs
@@ -9,12 +9,12 @@ namespace GraphExplorerMVC.Models
 {
     public class Asset
     {
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string RawUrl { get; set; }
         public string Published { get; set; }
+        public string Tag { get; set; }
         public virtual Application Application { get; set; }
 
     }
