976ab3d655b617a6379591f7d37a26fc429cfc91
David Bures <david@buresovi.com>
2015-10-23 08:52:16 -0700

    connected to api and deleted fake api

--- WeCanHelp/WeCanHelpOfficeAppWeb/App/Home/Main.js
+++ WeCanHelp/WeCanHelpOfficeAppWeb/App/Home/Main.js
@@ -7,11 +7,11 @@ angularApp.controller("Main", function ($scope, $http) {
 
     $http({
         method: 'GET',
-        url: 'http://localhost:61416/API/Values'
+        url: 'http://localhost:30883/odata/Assets?$filter=Application/Id%20eq%201%20and%20Published%20ne%20null'
     }).then(function successCallback(response) {
         // this callback will be called asynchronously
         // when the response is available
-        $scope.videos = response.data;
+        $scope.videos = response.data.value;
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
@@ -20,5 +20,5 @@ angularApp.controller("Main", function ($scope, $http) {
 });
 
 function play(e) {
-    $('#videoFrame').attr('src', e.href);
+    $('#videoFrame').attr('src', 'https://aka.ms/azuremediaplayeriframe?autoplay=false&url=' + encodeURIComponent(e.href));
 }
